470512f5d1543c559a2b5a465688a21c01e4e85d
Kyle Vekos <kvekos@gmail.com>
2019-02-14 11:03:37 -0500

    first commit

---
+++ GameOfLife.cpp
@@ -0,0 +1,17 @@
+﻿// GameOfLife.cpp : Defines the entry point for the application.
+//
+
+#include "GameOfLife.h"
+
+using namespace std;
+
+int main()
+{
+	string nc;
+
+	cout << "Hello CMake." << endl;
+
+	cin >> nc;
+
+	return 0;
+}
